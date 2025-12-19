270fd508fd9ceef7e460681bfc924dc0af8c0277
bala-amypo <bala@amypo.in>
2025-12-19 06:43:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/TicketServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Ticket;
+import com.example.demo.repository.TicketRepository;
+import com.example.demo.service.TicketService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TicketServiceImpl implements TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    // REQUIRED constructor order
+    public TicketServiceImpl(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    @Override
+    public Ticket createTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    @Override
