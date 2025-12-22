80a8e078a66c525ce2b24a759847e2450b55d80f
bala-amypo <bala@amypo.in>
2025-12-22 05:56:21 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -3,13 +3,14 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users" , uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "
     private String fullName;
 
     @Column(unique = true)
